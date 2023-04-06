6719ead1c5876aff6807bb0e2b72a52945fb69fd
BishalDe <itsbishalde@gmail.com>
2023-04-06 23:39:15 +0530

    django vercel_2

--- TaskMate/settings.py
+++ TaskMate/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ba%lp_6u^1lal%kl%navy75i&seo541#_8@oywy1unw-j9*a()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*",".vercel.app"]
 
 
 # Application definition
--- build_files.sh
+++
@@ -1,3 +0,0 @@
-# build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
