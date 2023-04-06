72188709973ccf4cdd633dc668e1e569b22a397c
BishalDe <itsbishalde@gmail.com>
2023-04-06 23:56:21 +0530

    fiexed vercel

--- TaskMate/settings.py
+++ TaskMate/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
