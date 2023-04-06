0f74b1d6338cdf9a0fc87d70b45bfe5260a4c6e3
BishalDe <itsbishalde@gmail.com>
2023-04-07 00:17:14 +0530

    fixed vercel_8

--- TaskMate/settings.py
+++ TaskMate/settings.py
@@ -80,13 +80,20 @@ WSGI_APPLICATION = 'TaskMate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'clustix',  
+        'USER': 'bishalde',  
+        'PASSWORD': '8299260163',  
+        'HOST': 'db4free.net',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
+}  
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
