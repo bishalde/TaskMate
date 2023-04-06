97851ba00232b51f08d331c6e9d68d62731b66fb
BishalDe <itsbishalde@gmail.com>
2023-04-07 00:09:33 +0530

    fixed vercel_5

--- TaskMate/settings.py
+++ TaskMate/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'TaskMate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
