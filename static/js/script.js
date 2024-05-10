2365e773b2539896cc81c8f715fefd49ff33e09d
TusharKumarChopra <tusharkumarchopra@gmail.com>
2024-05-10 22:56:33 +0530

    Added dark-mode feature

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,27 @@
+// script.js
 
+document.addEventListener('DOMContentLoaded', function() {
+    const crescentImage = document.querySelector('.optionsicon img[src$="/images/crescent.png"]');
+    const body = document.getElementById('main-body');
+    const header = document.querySelector('header');
+
+    // Check if the user has a stored preference for the theme
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+        body.classList.add(storedTheme);
+        // Add dark-mode class to header if the theme is dark
+        if (storedTheme == 'dark-mode') {
+            header.classList.add('header-dark');
+        }
+    }
+
+    crescentImage.addEventListener('click', function() {
+        // Toggle dark-mode class on body
+        body.classList.toggle('dark-mode');
+        // Toggle header-dark class on header
+        header.classList.toggle('header-dark', body.classList.contains('dark-mode'));
+        // Update the stored preference for the theme
+        const currentTheme = body.classList.contains('dark-mode') ? 'dark-mode' : '';
+        localStorage.setItem('theme', currentTheme);
+    });
+});
